0d0cefb6087577283eb67b5c1d0625d8031fad7e
Victor Manuel Acosta <138520518+VAcosta718@users.noreply.github.com>
2025-09-14 18:25:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,1 @@
-import java.util.Random;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        int playerChoice = scanner.nextInt();
-        int computerChoice =;
-
-        System.out.println("Welcome to Tic-Tac-Toe play at your own risk.");
-
-
-
-
-
-
-    }
-}
